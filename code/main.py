d3066631e25ee9287383531372cdace58aeb108f
@needy_sz <60481664+needysz@users.noreply.github.com>
2020-02-02 21:05:42 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,35 +1,41 @@
 import os
-from sys import exit
+from sys import exit 
 
-def dubbing():
-	files = os.listdir()
-	for dirfile in files:
-		delfile = open(dirfile, 'w')
-		delfile.close()
+class dub:
+	def dubbing():
+		files = os.listdir()
+		for dirfile in files:
+			cleanfile = open(dirfile, 'w')
+			cleanfile.close()
 
-def dubbing_filelist():
-	files = os.listdir()
-	print('INFO:\n')
-	for dirfiles in files:
-		print(dirfiles + ' - CLEANED')
+	def dubbing_filelist():
+		files = os.listdir()
+		print('INFO:\n')
+		for dirfiles in files:
+			cleanfile = open(dirfiles, 'r')
+			if os.path.getsize(dirfiles) > 0:
+				print(dirfiles, '- ERROR: NOT CLEANED')
+			elif os.path.getsize(dirfiles) == 0:
+				print(dirfiles, '- REPORT: CLEANED')
+			cleanfile.close()
 
-os.system('cls')						# Авторство
-print('\n$ FileTotal : Alpha 0.1\n$ AUTHOR : by @needy_sz\n')	# 
+os.system('cls')											# Авторство
+print('\n$ FileTotal : Alpha 0.1\n$ AUTHOR : by @needy_sz\n')						# 
 
-act = str(input('$> '))
+act = str(input('$> '))											# Выбор режима
 
-if act == 'mode dub' or act == 'dub mode':		# dubbing-mode
+if act == 'mode dub' or act == 'dub mode':								# dubbing-mode
 	print('<DUBBING MODE>')
 
-	path = str(input('\n$> path: '))
+	path = input('\n$> path: ')
 
-	try:
-		os.chdir(path)			# Переход по указанному пути
+	try
+		os.chdir(path)										# Переход по указанному пути
 	except FileNotFoundError:
 		print('\nERROR: [ INVALID PATH ]')
 		exit()
 
-	files = os.listdir()	# Присваивание переменной   
+	files = os.listdir()										# Присваивание переменной   
 
 	print('''
    @  PATH: {0}
@@ -40,9 +46,9 @@ if act == 'mode dub' or act == 'dub mode':		# dubbing-mode
 	act = str(input('$> '))
 
 	if act == 'start dub':
-		dubbing()
+		dub.dubbing()
 		print('\n[ DUBBING PROCESS HAS BEEN ENDED ]\n')
-		dubbing_filelist()
+		dub.dubbing_filelist()
 
 	elif act == 'undo' or 'undo()':
 		exit()
@@ -58,5 +64,5 @@ elif act == 'exit' or 'exit()':
 	exit()
 
 else:
-	print('\nERROR: [ INVALID COMMAND ]\n')
+	print('\nERROR: [ INVALID MODE ]\n')
 	exit()
